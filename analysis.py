e4bb9057a508ac42fa7db5627475477c397a3373
Ethan Hotz <etho4178@colorado.edu>
2026-07-14 15:07:04 -0600

    Added code for largest sale

--- analysis.py
+++ analysis.py
@@ -5,3 +5,8 @@ df = pd.read_csv("data.csv")
 print("Number of rows: ", len(df))
 print("Mean Price: ", df["Price"].mean())
 print( "Median Price: ", df["Price"].median())
+
+# Print the largest sale 
+print("Largest Sale: ", df["Price"].max())
+# Print the smallest sale
+print("Smallest Sale: ", df["Price"].min())
