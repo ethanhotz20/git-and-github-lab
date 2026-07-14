edeb1fc701287881eebe3a7329d8a6f36320a63d
Ethan Hotz <etho4178@colorado.edu>
2026-07-14 15:26:12 -0600

    fixed the label boss! yay!

--- analysis.py
+++ analysis.py
@@ -2,11 +2,11 @@ import numpy as np
 import pandas as pd
 
 df = pd.read_csv("data.csv")    
-print("Number of rows: ", len(df))
-print("Mean Price: ", df["Price"].mean())
-print("Median Price: ", df["Price"].median())
+print("NRows: ", len(df))
+print("M_Price: ", df["Price"].mean())
+print("Med_Price: ", df["Price"].median())
 
 # Print the largest sale 
-print("Largest Sale: ", df["Price"].max())
+print("Max_Price: ", df["Price"].max())
 # Print the smallest sale
-print("Smallest Sale: ", df["Price"].min())
+print("Min_Price: ", df["Price"].min())
