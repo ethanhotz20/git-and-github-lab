f25cb7a602304fa0372cd4593b5228e4eda520e3
Ethan Hotz <etho4178@colorado.edu>
2026-07-14 15:24:35 -0600

    Changed print labels to be more specific

--- analysis.py
+++ analysis.py
@@ -2,11 +2,11 @@ import numpy as np
 import pandas as pd
 
 df = pd.read_csv("data.csv")    
-print("Number of rows: ", len(df))
-print("Mean Price: ", df["Price"].mean())
-print("Median Price: ", df["Price"].median())
+print("Number of transactions: ", len(df))
+print("Mean Transaction Price: ", df["Price"].mean())
+print("Median Transaction Price: ", df["Price"].median())
 
 # Print the largest sale 
-print("Largest Sale: ", df["Price"].max())
+print("Largest Transaction: ", df["Price"].max())
 # Print the smallest sale
-print("Smallest Sale: ", df["Price"].min())
+print("Smallest Transaction: ", df["Price"].min())
